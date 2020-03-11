6878d9adf1e52f7c3410d6dd09b79d0a8f2e6cc7
邓伟彬 <gdoudeng@qq.com>
2020-03-11 14:26:31 +0800

    chore: 优化选择音频的样式

--- mediapicker/src/main/java/com/mingyuechunqiu/mediapicker/feature/main/detail/MediaPickerMainAdapter.java
+++ mediapicker/src/main/java/com/mingyuechunqiu/mediapicker/feature/main/detail/MediaPickerMainAdapter.java
@@ -2,12 +2,16 @@ package com.mingyuechunqiu.mediapicker.feature.main.detail;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+
 import android.view.View;
 import android.widget.CompoundButton;
 
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.mingyuechunqiu.mediapicker.R;
 import com.mingyuechunqiu.mediapicker.data.bean.MediaAdapterItem;
+import com.mingyuechunqiu.mediapicker.data.bean.MediaInfo;
+import com.mingyuechunqiu.mediapicker.data.constants.MediaPickerType;
 import com.mingyuechunqiu.mediapicker.feature.picker.MediaPicker;
 import com.mingyuechunqiu.mediapicker.ui.adapter.BaseMediaPickerAdapter;
 
@@ -46,15 +50,24 @@ class MediaPickerMainAdapter extends BaseMediaPickerAdapter<MediaAdapterItem, Ba
         helper.setChecked(R.id.cb_mp_media_item_check, item.isChecked())
                 .addOnClickListener(R.id.iv_mp_media_item_show);
         AppCompatImageView ivShow = helper.getView(R.id.iv_mp_media_item_show);
-        if (ivShow != null && item.getInfo() != null) {
-            MediaPicker.getImageEngine().showImage(mContext,
-                    new File(item.getInfo().getFilePath()),
-                    R.drawable.mp_media_placeholder, R.drawable.mp_media_error, ivShow);
+        MediaInfo itemInfo = item.getInfo();
+        if (ivShow != null && itemInfo != null) {
+            if (MediaPickerType.TYPE_AUDIO.equals(itemInfo.getType())) {
+                ivShow.setImageResource(R.drawable.music);
+            } else {
+                MediaPicker.getImageEngine().showImage(mContext,
+                        new File(itemInfo.getFilePath()),
+                        R.drawable.mp_media_placeholder, R.drawable.mp_media_error, ivShow);
+            }
+        }
+        if (itemInfo != null && MediaPickerType.TYPE_AUDIO.equals(itemInfo.getType())) {
+            AppCompatTextView textView = helper.getView(R.id.tv_mp_media_item_content);
+            textView.setText(itemInfo.getName().split(itemInfo.getSuffixType())[0]);
         }
         if (item.isChecked()) {
-            mSelectedList.add(item.getInfo());
+            mSelectedList.add(itemInfo);
         } else {
-            mSelectedList.remove(item.getInfo());
+            mSelectedList.remove(itemInfo);
         }
     }
 }
